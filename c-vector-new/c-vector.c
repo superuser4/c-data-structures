e77b0eb7941c19a732b652945aa51d73dc558909
supercat7 <superuser3344@protonmail.com>
2025-02-13 13:03:59 -0500

    works?

--- c-vector-new/c-vector.c
+++ c-vector-new/c-vector.c
@@ -6,45 +6,62 @@
 typedef struct {
     uint elem; // element number
     uint size; // size of array in bytes
-    void* buf[];
+    void** buf;
 } vector;
 
 void vector_init(vector* v) {
-    *v->buf = malloc(0);
-    v->elem = -1;
-    v->size = 0;
+    v->buf = malloc(sizeof(void*));
+    v->elem = 0;
+    v->size = sizeof(void*);
 }
 void vector_realloc(vector * v, uint size) {
     v->size += size;
-    realloc(*v->buf, v->size);
+    
+    void* temp = realloc(v->buf, v->size);
+        if (temp != NULL) {
+        v->buf = temp;
+    } else {
+        printf("Mem realloc failed!\n");
+        exit(1);
+    }
     
 }
 void vector_push(vector *v, void* element, uint size) {
-    vector_realloc(v, size);
+    vector_realloc(v, sizeof(void*));
+    
+    v->buf[v->elem] = malloc(size);
+    
+    memcpy(v->buf[v->elem], element, size);
     v->elem++;
-    v->buf[v->elem] = element;
 }
 void vector_pop() {}
 
 void vector_free(vector* v) {
-    free(*v->buf);
+    for (uint i=0;i<v->elem;i++) {
+        free(v->buf[i]);
+    }
+    free(v->buf);
+    v->size = 0;
+    v->elem = 0;
 }
 
 
 int main() {
     // demonstration
     vector v;
+    int a = 1; int b = 2; int c = 3;
     vector_init(&v);
-    vector_push(&v, (void*)1, sizeof(1)); // int vector
-    //vector_push(&v, (void*)2, sizeof(2));
-    //vector_push(&v, (void*)3, sizeof(3));
+    vector_push(&v, (void*)&a, sizeof(a)); // int vector
+    vector_push(&v, (void*)&b, sizeof(b));
+    vector_push(&v, (void*)&c, sizeof(c));
 
+
+    printf("Vector element number: %u\n", v.elem);
+    printf("Vector size: %u\n", v.size);
     for (uint i=0;i < v.elem; i++) {
         int num = *(int*)v.buf[i];
         printf("Vector elements: %d\n", num);
     }
-    printf("Vector element number: %u\n", v.elem);
-    printf("Vector size: %u\n", v.size);
 
     vector_free(&v);
     printf("Size after free: %d\n", v.size);
