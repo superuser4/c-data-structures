8effdb91c6ff4d70b809a57c244193980cd65feb
supercat7 <superuser3344@protonmail.com>
2025-03-27 12:45:24 -0400

    little refresh

--- c-vector-new/c-vector.c
+++ c-vector-new/c-vector.c
@@ -4,13 +4,14 @@
 #include "stdlib.h"
 #include "c-vector.h"
 
-void vector_init(vector* v, uint size) {
+void vector_init(vector* v, uint32_t size) {
     v->elem = 0;
     v->size = 0;
     v->type = size;
     v->buf = malloc(0);
 }
-void vector_realloc(vector * v, uint size) {
+
+void vector_realloc(vector * v, uint32_t size) {
     v->size += size;
     
     void* temp = realloc(v->buf, v->size);
@@ -25,7 +26,7 @@ void vector_realloc(vector * v, uint size) {
 
 void vector_free(vector* v) {
     // check if buf[i] is already freed
-    for (uint i=0; i<v->elem; i++) {
+    for (uint32_t i=0; i<v->elem; i++) {
         if (v->buf[i] != NULL) {free(v->buf[i]);}
     }
     free(v->buf);
@@ -33,7 +34,7 @@ void vector_free(vector* v) {
     v->elem = 0;
 }
 
-void vector_push_back(vector *v, void* element, uint size) {
+void vector_push_back(vector *v, void* element, uint32_t size) {
     if (v->size == v->elem * sizeof(void*)) {
         vector_realloc(v, sizeof(void*));
     }
@@ -43,7 +44,7 @@ void vector_push_back(vector *v, void* element, uint size) {
     v->elem++;
 }
 
-void vector_push_index(vector *v, void* element, uint size, uint index, uint shift) {
+void vector_push_index(vector *v, void* element, uint32_t size, uint32_t index, int shift) {
     void* temp = realloc(v->buf[index], size);
     if (temp == NULL) {
         fprintf(stderr, "realloc failure!\n");
@@ -53,15 +54,13 @@ void vector_push_index(vector *v, void* element, uint size, uint index, uint shi
     
     if (shift == 0) {
         memcpy(v->buf[index], element, size);
-
-    } else if (shift == 1) {
-        
+    } else {
         if (v->size == v->elem * sizeof(void*)) {
             vector_realloc(v, sizeof(void*));
         }
         
         v->buf[v->elem] = malloc(size);
-        for (uint i=v->elem; i > index; i--) {
+        for (uint32_t i=v->elem; i > index; i--) {
             if (v->type == 1) {
                 size_t new_size = strlen((char*)v->buf[i-1]) + 1;
                 void* tmp = realloc(v->buf[i], new_size);
@@ -70,16 +69,15 @@ void vector_push_index(vector *v, void* element, uint size, uint index, uint shi
                     exit(1);
                 }
                 v->buf[i] = tmp;
-
                 strncpy(v->buf[i], v->buf[i-1], new_size);
             } else {
                 memcpy(v->buf[i],v->buf[i-1], v->type);
             }
         }
-
         memcpy(v->buf[index], element, size);
         v->elem++;
-    }
+    } 
+    
 }
 
 void vector_pop_back(vector *v) {
@@ -88,13 +86,13 @@ void vector_pop_back(vector *v) {
     v->size -= sizeof(void*);
 }
 
-void vector_pop_index(vector *v, uint index) {
+void vector_pop_index(vector *v, uint32_t index) {
     if (v->size == v->elem * sizeof(void*)) {
         int x = (sizeof(void*));
         vector_realloc(v, -x);
     }
 
-    for (uint i=index; i < v->elem; i++) {
+    for (uint32_t i=index; i < v->elem; i++) {
         if (v->type == 1) {
             size_t new_size = strlen((char*)v->buf[i+1]) + 1;
             void* tmp = realloc(v->buf[i], new_size);
@@ -112,4 +110,10 @@ void vector_pop_index(vector *v, uint index) {
     v->buf[v->elem-1] = NULL;
     v->elem--;
     v->size -= sizeof(void*);
+}
+
+void vector_pop_all(vector *v) {
+    while (v->elem > 0) {
+        vector_pop_back(v);
+    }
 }
