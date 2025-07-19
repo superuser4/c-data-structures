ec2d4a6fdfce1723658f40ca77f8b7acb691ff9b
superuser4 <superuser3344@protonmail.com>
2025-07-19 14:15:32 -0400

    added queue in rust

--- queue-rs/src/main.rs
+++ queue-rs/src/main.rs
@@ -1,3 +1,44 @@
+#[derive(Debug)]
+struct Queue <T> {
+    list: Vec<T>, 
+}
+
+impl<T> Queue<T> {
+    fn new() -> Queue<T> {
+        Queue { 
+            list: Vec::new()
+        }
+    }
+    fn enqueue(&mut self, data: T) {
+        self.list.push(data);
+    }
+    fn dequeue(&mut self) -> Option<T> {
+        if self.list.is_empty() {
+            None
+        } else {
+            Some(self.list.remove(0))
+        }
+    }
+    fn is_empty(&self) -> bool {
+        self.list.is_empty()
+    }
+    fn top(&self) -> Option<&T> {
+       self.list.last() 
+    }
+    fn bottom(&self) -> Option<&T> {
+        self.list.first()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut queue: Queue<i32> = Queue::new();
+    queue.enqueue(10);
+    queue.enqueue(11);
+    queue.enqueue(12);
+    println!("Queue: {:?}", queue);
+    queue.dequeue();
+    println!("Queue: {:?}", queue);
+
+    println!("Empty?: {}", queue.is_empty());
+    println!("Top: {:?}\n Bottom: {:?}", queue.top(), queue.bottom());
 }
